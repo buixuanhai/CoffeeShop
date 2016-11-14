29c44c64114d7234c9c3b14acd2b68f2ebbb7753
Bui Xuan Hai <buixuanhai.it@gmail.com>
2016-11-14 09:18:33 +0700

    Add Receipt Detail Table Model (haven't finished yet)

---
+++ src/com/xuanhai/viewmodels/ReceiptDetailTableModel.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xuanhai.viewmodels;
+
+import com.xuanhai.models.HoaDon;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Admin
+ */
+public class ReceiptDetailTableModel extends AbstractTableModel {
+
+    private ReceiptRepository repo;
+    private List<HoaDon> data;
+    private List<Object[]> model;
+    private String[] columnNames = {"Id", "Ngày hóa đơn", "Giảm giá",
+        "Tổng trị giá", "Bàn", "Người lập"};
+
+    public ReceiptDetailTableModel() {
+        repo = new ReceiptRepository();
+        this.data = repo.get();
+        model = data.stream().map(e -> new Object[]{e.getHoaDonId(), e.getNgayHoaDon(), e.getGiamGia(), e.getTongTriGia(), e.getBan().getBanId(), e.getNhanVien()}).collect(Collectors.toList());
+    }
+
+    public ReceiptDetailTableModel(List<HoaDon> data) {
+        this.data = data;
+        model = data.stream().map(e -> new Object[]{e.getHoaDonId(), e.getNgayHoaDon(), e.getGiamGia(), e.getTongTriGia(), e.getBan().getBanId(), e.getNhanVien()}).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public int getRowCount() {
+        return model.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 6;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return model.get(rowIndex)[columnIndex];
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+}
